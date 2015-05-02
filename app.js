1ecc8b821f58a5bd6aedeea085e469d4c4b9bbc4
runningtalus <dave@davemay.org>
2015-05-02 16:47:52 -0600

    Setting up MongoDB and Mongoose.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+require('./app_server/models/db');
 var routes = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
 
---
+++ app_server/models/db.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://username:password@localhost:27027/Loc8r';
+
+mongoose.connect(dbURI);
