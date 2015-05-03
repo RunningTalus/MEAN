101f5f8055a0646b744fc5fd72ef997abaa9aa09
runningtalus <dave@davemay.org>
2015-05-03 11:24:49 -0600

    Mongoose and MongoDB operating nominally.

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,4 +1,15 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://username:password@localhost:27027/Loc8r';
+// var dbURI = 'mongodb://username:password@localhost:27027/Loc8r';
+var dbURI = 'mongodb://localhost/Loc8r';
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+}); 
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
 
 mongoose.connect(dbURI);
